74718a7a42d7ac498e23dd8f653b65e136bbbc15
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-15 17:24:43 +0200

    reset boiler plate

--- another.js
+++
@@ -1,5 +0,0 @@
-
-
-// this is a simple alert script.
-alert(`coding is super`);
-let name = prompt(`What is your name?`); 
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 
 // this is a simple alert script.
-alert(`coding is super`);
-let name = prompt(`What is your name?`); 
+// alert(`coding is super`);
+// let name = prompt(`What is your name?`); 
